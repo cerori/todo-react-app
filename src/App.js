65c4419e23b450bffb3e63e1b7f69e8503524493
박정근 <wjdrmsl@conia.co.kr>
2022-02-24 14:31:44 +0900

    추가, 수정, 삭제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Paper, List, Container } from "@material-ui/core";
+import Todo from './Todo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import AddTodo from "./AddTodo";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  add = (item) => {
+    const thisItem = this.state.items;
+    item.id = "ID-" + thisItem.length;
+    item.done = false;
+    thisItem.push(item);
+    this.setState({items: thisItem});
+    console.log("items: ", this.state.items);
+
+  }
+
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items)
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({items: newItems}, () => {
+      // 콜백
+      console.log("Update Items : ", this.state.items);
+    });
+  }
+
+  render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{margin: 16}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} delete={this.delete} />
+          ))}
+        </List>
+      </Paper>
+    )
+ 
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    const requestOptions = {
+      method: "GET",
+      headers: {"Content-type": "application/json"}
+    }
+
+    fetch("http://localhost:8080/todo", requestOptions)
+    .then((response) => response.json())
+    .then(
+      (response) => {
+        this.setState({items: response.data});
+      },
+      (error) => {
+        this.setState({
+          error
+        });
+      }
+    );
+  }
 }
 
 export default App;
